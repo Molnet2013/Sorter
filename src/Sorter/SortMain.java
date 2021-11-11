4c4771210c7351fa6dbbe560ec0bd659c2f794d7
Molnet2013 <winlof@hotmail.com>
2021-11-11 11:16:42 +0100

    Påbörjat for loop

--- src/Sorter/SortMain.java
+++ src/Sorter/SortMain.java
@@ -1,5 +1,17 @@
-public class App {
+package Sorter;
+
+public class SortMain {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    int[] intArgs = new int[args.length];
+        
+    for (int i = 0; i < args.length; i++) {
+        intArgs[i] = Integer.parseInt(null, [i]);
+    } 
+
+
+    // Sorter sorter = new Sorter();
+    // sorter.sort(args);
+        
     }
 }
