03fb99903d9f5c6a24ce1d4972f6bcbee4e1e756
Molnet2013 <winlof@hotmail.com>
2021-11-11 13:21:10 +0100

    Trying out with sending the list between the methods

--- src/Sorter/SortMain.java
+++ src/Sorter/SortMain.java
@@ -1,23 +1,18 @@
 package Sorter;
 
-import java.util.Arrays;
-
 public class SortMain {
     public static void main(String[] args) throws Exception {
 
         int[] intArgs = new int[args.length];
+        Sorter sorter = new Sorter();
 
         // for (int i : intArgs), skriver bara ut 0 0 0 0 0 0 0 76, skriver över med
         // sista värdet?
         for (int i = 0; i < args.length; i++) {
             intArgs[i] = Integer.parseInt(args[i]);
         }
-        Arrays.sort(intArgs);
 
-        System.out.println("Sorterad lista: ");
-        for (int number : intArgs) {
-            System.out.println("nummer: " + number);
-        }
+        sorter.sort(intArgs);
 
         // for (int i : intArgs) {
         // System.out.println(i);
--- src/Sorter/Sorter.java
+++ src/Sorter/Sorter.java
@@ -1,8 +1,18 @@
 package Sorter;
 
+import java.util.Arrays;
+
 public class Sorter {
 
-    public void sort(String[] args) {
-    }
+    public int[] sort(int[] unsorted) {
 
+        Arrays.sort(unsorted); // sorterar listan
+
+        // System.out.println("Sorterad lista: "); // bara printning
+        // for (int number : unsorted) {
+        // System.out.println("nummer: " + number);
+        // }
+        // int[] sortedList = unsorted;
+        return unsorted;
+    }
 }
