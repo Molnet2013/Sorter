4c096956b8e68b13b20c4de5c1ccdfd037442d31
Molnet2013 <winlof@hotmail.com>
2021-11-11 11:59:56 +0100

    loop to sort the list, tried to change first loop with no luck

--- src/Sorter/SortMain.java
+++ src/Sorter/SortMain.java
@@ -1,16 +1,27 @@
 package Sorter;
 
+import java.util.Arrays;
+
 public class SortMain {
     public static void main(String[] args) throws Exception {
 
         int[] intArgs = new int[args.length];
 
+        // for (int i : intArgs), skriver bara ut 0 0 0 0 0 0 0 76, skriver över med
+        // sista värdet?
         for (int i = 0; i < args.length; i++) {
             intArgs[i] = Integer.parseInt(args[i]);
         }
-        for (int i : intArgs) {
-            System.out.println(i);
+        Arrays.sort(intArgs);
+
+        System.out.println("Sorterad lista: ");
+        for (int number : intArgs) {
+            System.out.println("nummer: " + number);
         }
+
+        // for (int i : intArgs) {
+        // System.out.println(i);
+        // }
 
         // Sorter sorter = new Sorter();
         // sorter.sort(args);
